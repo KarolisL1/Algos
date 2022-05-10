12a72d1aa267586a08b569f86ec8adfdce36e856
karolisliepa <Karolis.Liepa@gmail.com>
2022-05-10 19:49:56 -0400

    Algos

---
+++ romantoint.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let result = 0;
+    s.split('').forEach((num, i) => {
+        if(roman[num] < roman[s[i + 1]]) result -= roman[num];
+        else result += roman[num];
+    });
+
+    return result;
+};
+
+console.log(romanToInt('MCMXCIV'))
---
+++ validParentheses.js
@@ -0,0 +1,22 @@
+var isValid = function(s) {
+    var stack = [];
+    var map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    for (var i = 0; i < s.length; i++) {
+        var c = s[i];
+        if (c in map) {
+            if (stack.length === 0 || stack.pop() !== map[c]) {
+                return false;
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+    return stack.length === 0;
+
+};
+
+console.log(isValid('()'));
